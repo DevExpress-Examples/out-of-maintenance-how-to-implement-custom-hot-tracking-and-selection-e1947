b69f4ae9479f22ef9ef9fad69fd01264d49deebe
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsApplication3/Form1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+// ...
+
+namespace WindowsApplication3 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void chartControl1_ObjectHotTracked(object sender, HotTrackEventArgs e) {
+            // Display the object's type.
+            this.Text = e.Object.GetType().ToString();
+            // Display the additional object's type, when it is available.
+            if (e.AdditionalObject != null) {
+                this.Text += " (" + e.AdditionalObject.GetType().ToString() + ")";
+            }
+        }
+
+        private void chartControl1_ObjectSelected(object sender, HotTrackEventArgs e) {
+            // Prevent the chart's diagram from being selected.
+            if (e.Object is XYDiagram) {
+                e.Cancel = true;
+            }
+            else {
+                MessageBox.Show(e.Object.GetType().ToString());
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to implement custom hot-tracking and selection
+
+
+<p>This example demonstrates how to implement hot-tracking and selecting for various chart elements.</p>
+
+<br/>
+
+
